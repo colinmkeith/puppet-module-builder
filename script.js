a4603fc78aeda2814f2878d692fc2c9557a6b65b
Colin Keith <colinmkeith@gmail.com>
2014-05-27 12:25:21 -0400

    script.js: added initial route controllers

--- script.js
+++ script.js
@@ -1,4 +1,33 @@
-var gepetoApp = angular.module('gepeto', []);
+var gepetoApp = angular.module('gepeto', [ 'ngRoute' ]);
+
+gepetoApp.config(function($routeProvider) {
+  $routeProvider
+
+  .when('/', {
+    templateUrl : 'pages/home.html',
+    controller  : 'mainController'
+  })
+
+  .when('/about', {
+    templateUrl : 'pages/about.html',
+    controller  : 'aboutController',
+  })
+
+  .when('/contact', {
+    templateUrl : 'pages/contact.html',
+    controller  : 'contactController'
+  })
+  ;
+});
+
 gepetoApp.controller('mainController', function($scope) {
-  $scope.message = 'Hello';
+  $scope.message = 'Hello World, main controller';
+});
+
+gepetoApp.controller('aboutController', function($scope) {
+  $scope.message = 'about Handler';
+});
+
+gepetoApp.controller('contactController', function($scope) {
+  $scope.message = 'contact us';
 });
